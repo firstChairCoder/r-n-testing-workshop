bbeff7e8009b8a068154c32c60825d665d831deb
firstChairCoder <md1qq7@gmail.com>
2022-06-21 15:49:59 +0100

    chore: make changes

--- prettier.config.js
+++ prettier.config.js
@@ -2,8 +2,16 @@ const options = {
   arrowParens: "avoid",
   singleQuote: false,
   bracketSpacing: true,
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: "always",
+  quoteProps: "as-needed",
+  requirePragma: false,
   semi: true,
   tabWidth: 2,
   trailingComma: "all",
+  useTabs: true,
 };
 module.exports = options;
